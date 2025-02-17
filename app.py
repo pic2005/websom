4acc54a2feff7b8d2e6ec054396fe512516f81f1
Pichaya Songtis <pichayasongtis@gmail.com>
2025-02-17 19:20:00 +0700

    Sign Up

--- app.py
+++ app.py
@@ -1,34 +1,61 @@
-from flask import Flask, render_template, redirect, url_for
-from forms import LoginForm
+from flask import Flask, render_template, redirect, url_for, request, flash
+from flask_sqlalchemy import SQLAlchemy
+from forms import LoginForm, RegisterForm
+from werkzeug.security import generate_password_hash, check_password_hash
 import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../instance/database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 # Ensure the templates folder is correctly referenced
 app.template_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 
-# ...existing code...
+# Import models
+from models.user import User
+from models.data import Tag
+
+# Create database tables
+with app.app_context():
+    db.create_all()
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     form = LoginForm()
     website_name = "Women's Stories"
     if form.validate_on_submit():
-        # Add your authentication logic here
         return redirect(url_for('home'))
     return render_template('home.html', form=form, website_name=website_name)
-
-# ...existing code...m)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        # Add your authentication logic here
-        return redirect(url_for('home'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and check_password_hash(user.password, form.password.data):
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        existing_user = User.query.filter_by(username=form.username.data).first()
+        if existing_user:
+            flash('Username already taken. Please choose another one.', 'danger')
+        else:
+            hashed_password = generate_password_hash(form.password.data, method='sha256')
+            new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Your account has been created! You can now log in.', 'success')
+            return redirect(url_for('login'))
+    return render_template('register.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
